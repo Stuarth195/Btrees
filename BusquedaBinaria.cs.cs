3983e06e94e2800141a80d5222576429fcc51a51
Stuarth195 <167147559+Stuarth195@users.noreply.github.com>
2025-05-03 20:39:17 -0600

    Listo
    Listo

---
+++ BusquedaBinaria.cs.cs
@@ -0,0 +1,31 @@
+﻿namespace Busquedas;
+
+public class BusquedaBinaria
+{
+    public static int Buscar(int[] arreglo, int valor)
+    {
+        int izquierda = 0, derecha = arreglo.Length - 1;
+
+        while (izquierda <= derecha)
+        {
+            int medio = (izquierda + derecha) / 2;
+
+            if (arreglo[medio] == valor)
+                return medio;
+            else if (valor < arreglo[medio])
+                derecha = medio - 1;
+            else
+                izquierda = medio + 1;
+        }
+
+        return -1;
+    }
+
+    // Método Main como punto de entrada para la ejecución
+    public static void Main()
+    {
+        int[] arreglo = { 1, 3, 5, 7, 9 };
+        int resultado = Buscar(arreglo, 7);
+        Console.WriteLine("Índice encontrado: " + resultado);
+    }
+}
---
+++ BusquedaLineal.cs
@@ -0,0 +1,14 @@
+﻿namespace Busquedas;
+
+public class BusquedaLineal
+{
+    public static int Buscar(int[] arreglo, int valor)
+    {
+        for (int i = 0; i < arreglo.Length; i++)
+        {
+            if (arreglo[i] == valor)
+                return i;
+        }
+        return -1;
+    }
+}
